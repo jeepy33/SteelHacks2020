48703ddfdd13e39502a150334a6f97bc181fffd4
tylercomisky <tdcomisky@gmail.com>
2020-02-14 17:59:59 -0500

    Renamed ViewControllers and segues

---
+++ recyclerewards/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  recyclerewards
+//
+//  Created by Tyler Comisky on 2/14/20.
+//  Copyright © 2020 Tyler Comisky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
