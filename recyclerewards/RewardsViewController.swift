e940c79cac1a058e68e677f3231109d477016783
tylercomisky <tdcomisky@gmail.com>
2020-02-15 15:56:13 -0500

    fixed icon scaling

---
+++ recyclerewards/RewardsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RewardsViewController.swift
+//  recyclerewards
+//
+//  Created by Tyler Comisky on 2/15/20.
+//  Copyright © 2020 Tyler Comisky. All rights reserved.
+//
+
+import UIKit
+
+class RewardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = false
+    }
+
+
+}
