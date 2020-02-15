8fe84f5712360d817e0c64f285ff705dfed683ff
alexaspaventa <alexaspaventa@gmail.com>
2020-02-14 20:50:31 -0500

    Fixed full screen

--- recyclerewards/LoginViewController.swift
+++ recyclerewards/LoginViewController.swift
@@ -15,6 +15,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = true
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
     }
